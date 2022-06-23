d79906b8c2fea6cfb7bd1f7adc33f85053dfb7b6
Caleb Holm <calebcholm@gmail.com>
2022-06-22 20:40:53 -0500

    Completed 35.5

--- src/app.js
+++ src/app.js
@@ -2,58 +2,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const pastes = require('./data/pastes-data');
+const pastesRouter = require('./pastes/pastes.router');
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
-app.use('/pastes/:pasteId', (req, res, next) => {
-  const { pasteId } = req.params;
-  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
-
-  if (foundPaste) {
-    res.json({ data: foundPaste });
-  } else {
-    next({ status: 404, message: `Paste id not found: ${pasteId}` });
-  }
-});
-
-//get pastes handler
-app.get('/pastes', (req, res) => {
-  res.json({ data: pastes });
-});
-
-//post pastes handler
-// Variable to hold the next ID
-// Because some IDs may already be used, find the largest assigned ID
-function bodyHasTextProperty(req, res, next) {
-  const { data: { text } = {} } = req.body;
-  if (text) {
-    return next();
-  }
-  next({
-    status: 400,
-    message: "A 'text' property is required.",
-  });
-};
-
-let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
-
-app.post(
-  "/pastes",
-  bodyHasTextProperty,
-  (req, res) => {
-  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
-    const newPaste = {
-      id: ++lastPasteId, // Increment last ID, then assign as the current ID
-      name,
-      syntax,
-      exposure,
-      expiration,
-      text,
-      user_id,
-    };
-    pastes.push(newPaste);
-    res.status(201).json({ data: newPaste });
-  }
-);
+//use pastes router
+app.use('/pastes', pastesRouter);
 
 // Not found handler
 app.use((req, res, next) => {
